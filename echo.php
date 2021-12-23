4372c03299258fb74a4e29c18ffbcc7ea23cd7ae
Ag3ntQ <95140254+Ag3ntQ@users.noreply.github.com>
2021-12-23 15:45:37 +0530

    Add files via upload

---
+++ echo.php
@@ -0,0 +1,11 @@
+<?php
+
+    echo "Hello PHP";
+
+    // this is single line comment
+
+  echo "</br><h1>Bie</h1>"
+
+    /* multiline comment */
+
+?>
---
+++ var.php
@@ -0,0 +1,13 @@
+<?php
+       // Basic Var
+   $name = "Ag3ntQ";
+   echo "Name : $name";
+     
+     // Constants
+   // define(name, value, case-insensitive)
+  echo "</br>";
+  define("age",17);
+  echo "Age ".age;
+  /* No dollar sign ($) is necessary before the constant name */
+
+?>
