b2435dc9147f7131abfd7477293d86a5a32b4551
Ag3ntQ <95140254+Ag3ntQ@users.noreply.github.com>
2021-12-23 16:31:13 +0530

    Add files via upload

---
+++ arithmetic.php
@@ -0,0 +1,36 @@
+<?php
+    // Arithmetic operations
+   
+   /*
+   Addition  $a + $b
+   Subtraction $a - $b
+   multiplication $a * $b
+   Divition $a / $b
+   Modulas $a % $b
+   */
+  
+  $a = 5;
+  $b = 6;
+  echo "A :".$a;
+  echo "</br>";
+  echo "B :".$b;
+   echo "</br>";
+  echo "Sum : ".$a+$b;
+     echo "</br>";
+ 
+ //  Data type
+  $a="11";
+  $b=9;
+  echo $a+$b;
+      echo "</br>";
+  /* PHP automatically converts each variable to the correct data type, according to its value. This is why the variable $str is treated as a number in the addition*/
+ 
+ // Boolean
+  $x = true; $y = false;
+  echo $x;
+
+    /*  
+  $x++; // equivalent to $x = $x+1;
+  $x--; // equivalent to $x = $x-1;
+*/
+?>
